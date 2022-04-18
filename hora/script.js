16898decb736963723ed57702d41c9ae78aac397
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2022-04-18 18:00:34 -0300

    correcao script

--- hora/script.js
+++ hora/script.js
@@ -1,6 +1,6 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('image,')
+    var img = window.document.getElementById('image')
     var data = new Date()
     var hora = data.getHours()
     //var hora = 22;
